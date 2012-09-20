5b7820a8b41782a65829d5a2fc88572c623f2bcc
Ulisses Albuquerque <ulisses.montenegro@gmail.com>
2012-09-20 14:27:01 -0300

    Added support for multiple comparison criteria

--- scripts/difftool.rb
+++ scripts/difftool.rb
@@ -13,16 +13,24 @@ module ROMHacking
       load_files(files)
     end
     
-    def compare(criteria = '!=')
+    def compare(*criteria)
+      # Check the number of criteria provided, it must match either (@files.size - 1)
+      # or be a single criteria
+      unless criteria.size == 1 || criteria.size == (@files.size - 1)
+        throw ArgumentError.new('You must provide either a single criteria or one for each file pair')
+      end
+      
       # Initially all the bytes in the file contents are interesting
       byte_range = (0..(@files.first[:data].length - 1)).to_a
       (0..(@files.size - 2)).each do |index|
         data_a = @files[index + 0].data
         data_b = @files[index + 1].data
+        data_op = criteria[index] || criteria.first
         
         # We filter all offsets that do not match the comparison criteria
         next_range = byte_range.find_all do |offset|
-          data_b[offset].send(criteria.to_sym, data_a[offset])
+          $stderr.puts("#{data_b[offset]} #{data_op} #{data_a[offset]}")
+          data_b[offset].send(data_op.to_sym, data_a[offset])
         end
         byte_range = next_range
       end
